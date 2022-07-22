541ffe1da844c85f101d1779e6e56f454ba535a1
Intuneteq <97626624+Intuneteq@users.noreply.github.com>
2022-07-22 17:33:16 +0800

    correction

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import "./Checkout.scss";
-import { Images } from "../../Constants";
 import { urlFor } from "../../lib/client";
 import { useStateContext } from "../../context/StateContext";
 
